9c9e59daa79949d55d5b1df09f886657c1d33ce1
Ahmet Hamza Emra <hamza.emra@gmail.com>
2016-09-29 14:08:38 -0500

    Create bubbleSort.py

---
+++ bubbleSort.py
@@ -0,0 +1,11 @@
+def bubbleSort(ar):
+    not_sorted=True
+    while(not_sorted):
+        not_sorted=False
+        for i in range(1,len(ar)-1):
+            if (ar[i]<ar[i-1]):
+                temp=ar[i]
+                ar[i]=ar[i-1]
+                ar[i-1]=temp
+                not_sorted=True
+    return ar
