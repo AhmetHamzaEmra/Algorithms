4941d27e03919fd722796d9464b2d39b145adca3
Ahmet Hamza Emra <hamza.emra@gmail.com>
2016-10-04 14:22:41 -0500

    Update bubbleSort.py

--- bubbleSort.py
+++ bubbleSort.py
@@ -2,7 +2,7 @@ def bubbleSort(ar):
     not_sorted=True
     while(not_sorted):
         not_sorted=False
-        for i in range(1,len(ar)-1):
+        for i in range(1,len(ar)):
             if (ar[i]<ar[i-1]):
                 temp=ar[i]
                 ar[i]=ar[i-1]
