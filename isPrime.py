04cf902e59a38e279f93f479c9b96e9a4b937c9b
Ahmet Hamza Emra <hamza.emra@gmail.com>
2016-12-06 14:35:25 -0600

    Checks if the number is prime

---
+++ isPrime.py
@@ -0,0 +1,13 @@
+def IsPrime(n):
+    
+    if n==1 or n%2==0:
+        return False
+    else:
+        i=3
+        while i*i<=n:
+            if n%i==0:
+                return False
+                break
+            i+=2
+    return True
+    
